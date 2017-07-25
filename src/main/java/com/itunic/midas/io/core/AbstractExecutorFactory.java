cee5149b88393e7ab5a6ce57f63a846bbeed07b6
yinbin <yinbin@huak.com.cn>
2017-07-25 16:46:39 +0800

    1

--- src/main/java/com/itunic/midas/io/core/AbstractExecutorFactory.java
+++ src/main/java/com/itunic/midas/io/core/AbstractExecutorFactory.java
@@ -1,11 +1,7 @@
 package com.itunic.midas.io.core;
 
-
 public abstract class AbstractExecutorFactory<T> {
-	/**
-	 * cpuºËÐÄÊý
-	 */
-	public  int CPU_CORES = Runtime.getRuntime().availableProcessors() * 2;
+	public int CPU_CORES = Runtime.getRuntime().availableProcessors() * 2;
 
-	public abstract  T getExecutor();
+	public abstract T getExecutor();
 }
